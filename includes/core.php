9ed740fff57345ce2bb55fde7fc5b48c23d581fc
Pedro Markun <pedro@esfera.mobi>
2012-11-22 04:21:08 -0200

    adicionado a ficha e o ibge, clean up no codigo

--- includes/core.php
+++ includes/core.php
@@ -1,6 +1,38 @@
 <?
-
 #### Custom Post Types
+global $ibge_setup, $ficha_setup;
+$ibge_setup = array(
+	'A187' => 'Possui plano?',
+	'A171' => 'Sistema Municipal de Ensino',
+	'A219' => 'Fundo Municipal de Educação',
+	'A211' => 'Conselho Municipal de Educação'
+	);
+
+
+$ficha_setup = array(
+    'Q1' => '1. Seu município possui plano de educação em vigência?',
+    'Q2' => '2. Quando o plano de educação foi aprovado?',
+    'Q3' => '3. O plano de educação já foi revisado nos últimos quatro anos?',
+    'Q4' => '4. Seu município está elaborando um plano de educação? [Somente para quem respondeu Não na pergunta 01]',
+    'Q5' => '5. Seu município pretende elaborar um plano de educação nesta gestão (2013-2016)? [Somente para quem respondeu Sim na pergunta 04]',
+    'Q6' => '6. Seu município pretende revisar o plano de educação nesta gestão (2013-2016)? [Somente para quem já tem plano]',
+    'Q7' => '7. Em qual momento da elaboração se encontra? (Somente para os munícipios que estão em processo de elaboração do Plano de Educação)',
+    'Q8' => '8. O município contratou ou pretende contratar algum tipo de consultoria externa para elaboração do plano de educação? [Para todos]',
+    'Q9' => '9. Quais dos órgãos/instâncias abaixo participam ou participaram da elaboração do plano de educação?',
+    'Q10' => '10. Quais das organizações/movimentos abaixo participam ou participaram da elaboração do plano de educação?',
+    'Q11' => '11. Dos segmentos da comunidade escolar descritos abaixo, quais participaram ou estão participando da elaboração do plano de educação de seu município.',
+    'Q12' => '12. Quais foram ou estão sendo os dados utilizados para a elaboração do diagnóstico do município a ser utilizado no plano de educação? [Se necessário, assinale mais de uma alternativa]',
+    'Q13' => '13. Quais foram ou estão sendo as principais metodologias utilizadas para a elaboração do plano de educação de seu município?',
+    'Q14' => '14. O processo de elaboração do plano de educação mobilizou ou vem mobilizando',
+    'Q15' => '15. Houve ou há um investimento na comunicação sobre o processo de construção/revisão do Plano?',
+    'Q16' => '16. Caso positivo, a comunicação do processo se deu:',
+    'Q17' => '17. Seu município está preparado para cumprir a lei de acesso à informação pública (lei 12.527/2011) com relação à área de educação?',
+    'Q18' => '18. Além da gestão municipal, participam ou participaram do processo de construção/revisão dos Planos',
+    'Q19' => '19. A construção do plano envolveu/envolve as seguintes etapas/modalidades e níveis da educação?',
+    'Q20' => '20. Houve ou há um investimento na participação de crianças e adolescentes na construção do Plano de Educação?',
+    'Q21' => '21.  Se sim, de que forma?'
+);
+
 
 add_action('init', 'municipio_register');
  
@@ -23,37 +55,103 @@ function municipio_register() {
 
 /* Create one or more meta boxes to be displayed on the post editor screen. */
 function add_municipio_metaboxes() {
+	 add_meta_box('post-municipio-ibge', 'Munic IBGE', 'municipio_post_ibge_metabox', 'municipio', 'normal', 'default');
 	 add_meta_box('post-municipio-ficha', 'Ficha do Munícipio', 'municipio_post_ficha_metabox', 'municipio', 'normal', 'default');
 	 remove_meta_box( 'slugdiv', 'municipio', 'normal' );
 	 remove_meta_box( 'submitdiv', 'municipio', 'normal' );
 }
 
-/* Display the post meta box. */
+/* Display the post IBGE meta box. */
+function municipio_post_ibge_metabox( $object, $box ) {
+    global $post, $ibge_setup;
+    // Noncename needed to verify where the data originated
+    echo '<input type="hidden" name="ibgemeta_noncename" id="ibgemeta_noncename" value="' .
+    wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+    
+    // Get the location data if its already been entered
+	$ibge_results = array();
+	
+	foreach ($ibge_setup as $key => $value) {
+	    $ibge_results[$key] = get_post_meta($post->ID, $key, true);
+	}
+	
+	
+    // Echo cria o field
+    echo '<p>Possui plano?</p>';
+    echo '<input type="text" name="A187" value="' . $ibge_results['A187']  . '" class="widefat" />';
+    echo '<p>Instâncias de Gestão Democrática</p>';
+    echo '<p>Sistema Municipal de Ensino</p>';
+    echo '<input type="text" name="A171" value="' . $ibge_results['A171']  . '" class="widefat" />';
+    echo '<p>Fundo Municipal de Educação</p>';
+    echo '<input type="text" name="A219" value="' . $ibge_results['A219']  . '" class="widefat" />';
+    echo '<p>Conselho Municipal de Educação</p>';
+    echo '<input type="text" name="A211" value="' . $ibge_results['A211']  . '" class="widefat" />';
+}
+
+// Save the Metabox Data
+function municipio_post_save_ibge_meta($post_id, $post) {
+    global $ibge_setup;
+    // verify this came from the our screen and with proper authorization,
+    // because save_post can be triggered at other times
+    if ( !wp_verify_nonce( $_POST['ibgemeta_noncename'], plugin_basename(__FILE__) )) {
+    return $post->ID;
+    }
+    // Is the user allowed to edit the post or page?
+    if ( !current_user_can( 'edit_post', $post->ID ))
+        return $post->ID;
+    // OK, we're authenticated: we need to find and save the data
+    // We'll put it into an array to make it easier to loop though.
+    
+    //Possivel combinar as funcoes??
+    $ibge_meta = array();
+    foreach ($ibge_setup as $key => $value) {
+	    $ibge_meta[$key] = $_POST[$key];
+	}
+    
+    // Add values of $events_meta as custom fields
+    foreach ($ibge_meta as $key => $value) { // Cycle through the $events_meta array!
+        if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+        $value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+        if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+            update_post_meta($post->ID, $key, $value);
+        } else { // If the custom field doesn't have a value
+            add_post_meta($post->ID, $key, $value);
+        }
+        if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+    }
+}
+
+
+
+/* Display the post ficha meta box. */
 function municipio_post_ficha_metabox( $object, $box ) {
-        global $post;
+    global $post, $ficha_setup;
     // Noncename needed to verify where the data originated
     echo '<input type="hidden" name="fichameta_noncename" id="fichameta_noncename" value="' .
     wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+    
     // Get the location data if its already been entered
-        $ficha_A187 = get_post_meta($post->ID, 'A187', true);
-	$ficha_A171 = get_post_meta($post->ID, 'A171', true);
-	$ficha_A219 = get_post_meta($post->ID, 'A219', true);
-	$ficha_A211 = get_post_meta($post->ID, 'A211', true);
+	$ficha_results = array();
 	
-    // Echo out the field
-    echo '<p>Possui plano?</p>';
-    echo '<input type="text" name="A187" value="' . $ficha_A187  . '" class="widefat" />';
-    echo '<p>Instâncias de Gestão Democrática</p>';
-    echo '<p>Sistema Municipal de Ensino</p>';
-    echo '<input type="text" name="A171" value="' . $ficha_A171  . '" class="widefat" />';
-    echo '<p>Fundo Municipal de Educação</p>';
-    echo '<input type="text" name="A219" value="' . $ficha_A219  . '" class="widefat" />';
-    echo '<p>Conselho Municipal de Educação</p>';
-    echo '<input type="text" name="A211" value="' . $ficha_A211  . '" class="widefat" />';
+	foreach ($ficha_setup as $key => $value) {
+	    $ficha_results[$key] = get_post_meta($post->ID, $key, true);
+	}
+	
+	
+    // Echo cria o field 
+    //echo '<p>Ficha</p>';
+    //echo '<input type="text" name="A187" value="' . $ficha_results['A187']  . '" class="widefat" />';
+
+    //Looping cria field
+    foreach ($ficha_setup as $key => $value) {
+	  echo "<p>" . $value . "</p>";
+	  echo "<input type='text' name='" . $key . "' value='" . $ficha_results[$key] . "' class='widefat' />";
+	}
 }
 
 // Save the Metabox Data
 function municipio_post_save_ficha_meta($post_id, $post) {
+    global $ficha_setup;
     // verify this came from the our screen and with proper authorization,
     // because save_post can be triggered at other times
     if ( !wp_verify_nonce( $_POST['fichameta_noncename'], plugin_basename(__FILE__) )) {
@@ -64,9 +162,12 @@ function municipio_post_save_ficha_meta($post_id, $post) {
         return $post->ID;
     // OK, we're authenticated: we need to find and save the data
     // We'll put it into an array to make it easier to loop though.
-    $ficha_meta['A187'] = $_POST['A187'];
-    $ficha_meta['_ficha_vigencia'] = $_POST['_ficha_vigencia'];
     
+    //Possivel combinar as funcoes??
+    $ficha_meta = array();
+    foreach ($ficha_setup as $key => $value) {
+	    $ficha_meta[$key] = $_POST[$key];
+	}
     
     // Add values of $events_meta as custom fields
     foreach ($ficha_meta as $key => $value) { // Cycle through the $events_meta array!
@@ -80,6 +181,8 @@ function municipio_post_save_ficha_meta($post_id, $post) {
         if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
     }
 }
+
+add_action('save_post', 'municipio_post_save_ibge_meta', 1, 2); // save the custom fields
 add_action('save_post', 'municipio_post_save_ficha_meta', 1, 2); // save the custom fields
 
 ?>
